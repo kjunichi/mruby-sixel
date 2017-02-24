0f51c89ced2c6327e807ec54e1aae59195e37cf0
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-02-24 17:03:52 +0900

    Update mrb_sixel.rb

--- test/mrb_sixel.rb
+++ test/mrb_sixel.rb
@@ -3,15 +3,6 @@
 ##
 
 assert("Sixel#hello") do
-  t = Sixel.new "hello"
+  t = SixelEncoder.new 
   assert_equal("hello", t.hello)
-end
-
-assert("Sixel#bye") do
-  t = Sixel.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("Sixel.hi") do
-  assert_equal("hi!!", Sixel.hi)
 end
